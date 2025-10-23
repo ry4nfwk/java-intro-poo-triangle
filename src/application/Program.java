a955917869d18259aae1d8b298fb7e3538e35cfa
ry4nfwk <ry4nn47@icloud.com>
2025-10-23 11:17:41 -0300

    first commit

---
+++ src/application/Program.java
@@ -0,0 +1,28 @@
+package application;
+
+import entities.Triangle;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Triangle x,y;
+       x = new Triangle();
+       y = new Triangle();
+       System.out.println("Enter the measures of triangle X");
+       x.a = sc.nextDouble();
+       x.b = sc.nextDouble();
+       x.c = sc.nextDouble();
+       System.out.println("Enter the measures of triangle Y");
+       y.a = sc.nextDouble();
+       y.b = sc.nextDouble();
+       y.c = sc.nextDouble();
+       System.out.printf("Triangle X area: %.4f\n", x.area());
+       System.out.printf("Triangle Y area: %.4f\n", y.area());
+       if (x.area() > y.area()){
+           System.out.println("Larger area: X");
+        } else{
+           System.out.println("Larger area: Y");
+        }
+    }
+}
